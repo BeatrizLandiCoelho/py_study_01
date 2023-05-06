85dd2a3195e4526561e5b5eb135a477cf6a11543
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-06 01:15:04 -0300

    sintaxe function

---
+++ functions/app.py
@@ -0,0 +1,16 @@
+def nome_function():
+        #tudo dentro do tab esta dentro da functio
+        print("retorno function") 
+
+nome_function()
+
+def function_generic_name(parametro):
+        print(f"print parametro {parametro}")
+
+function_generic_name("isso é um parametro")
+        
+
+def caalculate_numbers(n1,n2):
+        result = n1 + n2
+        return result
+print(caalculate_numbers(1,2))
