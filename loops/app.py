7b813c8a2e6e37abc6c04d5b4ed05fad55284a35
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-06 21:14:53 -0300

    a

---
+++ loops/app.py
@@ -0,0 +1,31 @@
+i=1
+
+while i < 10:
+    print(i)
+    i += 1
+
+print("finish")   
+
+#________________________________________________________________________
+kids = ["johan", "anne", "akaza","kaory"]
+
+for item in kids:
+    print(item)
+
+speling = "speling"
+
+for letter in speling:
+    print(letter)
+
+#range tell me whe stop
+for index in range(20):
+    print(index)
+
+#the fisrt number when you have more than one parmaether will tell when it starts
+for index in range(10,20):
+    print(index)
+
+#the second number when you have more than two parmaether will tell when have an gap
+for index in range(10,20,2):
+    print(index)
+#____________________________________________________________________________________
