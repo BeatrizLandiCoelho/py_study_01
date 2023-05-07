f193705cc026d7447942fc33dacd5bebab80e777
BeatrizLandi <beatriz.landi.coelho@gmail.com>
2023-05-06 22:04:49 -0300

    set dict

---
+++ dict/app.dict.py
@@ -0,0 +1,25 @@
+#_____they hace keys and values
+
+months ={
+
+    "jan":"january",
+    "feb":"febuary",
+    "mar":"mars",
+    "apr":"apryl",
+    "may":"may",
+    "jun":"June",
+    "jl":"July",
+    "aul":"algust",
+    "set":"setember",
+    "oct":"octuber",
+    "nov":"november",
+    "dez":"dezember"
+
+}
+
+print(months["dez"])
+
+print(months.get("nov"))
+
+#you can pass a second param, the secont param will be the default in case the firt one arent found
+print(months.get("abc","default"))
---
+++ set/app.py
@@ -0,0 +1,18 @@
+#sets are un ordenate list jsons
+
+fruts = {"apple","orange", "pinapple"}
+
+print(fruts)
+
+fruts.add("grape")
+
+#remove an specif item on set
+fruts.remove("orange")
+
+print(fruts)
+
+#remove last item from set
+fruts.pop()
+
+
+
